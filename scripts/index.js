61e1314ef5bedf9362d6dadf00fafe2cc39a12bf
George Colon <geocolon@gmail.com>
2018-11-25 13:11:58 -0800

    Updated with new content

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,53 @@
+'use strict';
 function bookSearch(){
-  var search = document.getElementById('search').value
+  var search = document.getElementById('search').value;
 
-  document.getElementById('results').innerHTML = "",
+  document.getElementById('results').innerHTML = '',
 
   console.log(search);
 
   $.ajax({
-    url: "https://www.googleapis.com/books/v1/volumes?q=" + search,
-    dataType: "json",
+    url: 'https://www.googleapis.com/books/v1/volumes?q=' + search,
+    dataType: 'json',
     success: function(data) {
+      console.log(data);
       for(let i = 0; i < data.items.length; i++){
         let book = data.items[i].volumeInfo;
         let image = data.items[i].volumeInfo.imageLinks.thumbnail;
         let addressLink = data.items[i].volumeInfo.infoLink;
         results.innerHTML += 
-        `<div class="col-4">
+        `
+        <div class="col-3">
+        <div class="list-content">
         <div class="class="${typeof image === 'undefined' ? 'hidden' : ''}">
         <img src="${image}" alt="${book.value}">
         </div>
         <h4>${book.title}</h4>
-        <p>${book.authors}</p>
-        <div class="desc">
+        <p>by ${book.authors}</p>
+        <button onclick="buttonHide('desc${i}')">description</button>
+        <div id="desc${i}" class="desc">
         <p>${book.description}</p>
         </div>
-        <a href="${addressLink}">More info.</a>
+        <a href="${addressLink}"><button>More info.</button></a>
         </div>
-        `
+        </div>
+        `;
       }
     },
     type: 'GET'
   });
 }
 
-document.getElementById('button').addEventListener('click', bookSearch, false);
+const buttonHide = (iD) => {
+  let x = document.getElementById(iD);
+  console.log('Is is working', x);
+
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+};
+
+document.getElementById('button').addEventListener('click', bookSearch, buttonHide, false);
+

7ae541c7191e8693e5fdfaa5de41cc7e6299e229
George Colon <geocolon@gmail.com>
2018-11-25 21:32:32 -0800

    I need to updated Mix Content error

--- scripts/index.js
+++ scripts/index.js
@@ -13,7 +13,7 @@ function bookSearch(){
 
 
   $.ajax({
-    url: 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&max-results=40',
+    url: 'https://www.googleapis.com/books/v1/volumes?q=' + search + '&max-results=40&start-index=0',
     dataType: 'json',
     success: function(data) {
       console.log('This is the data: ',data);
